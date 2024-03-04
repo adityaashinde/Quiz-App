79085674ac0240bd36f68fde121a3b87749d7250
adityaashinde <adityashinde4580@gmail.com>
2024-03-04 22:38:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: QuizApp(),
+    );
+  }
+}
+
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
+
+  @override
+  State<QuizApp> createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Quizz App",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
