2d2bbd8cbe32bef1ef7aeef9495229a9b5974e66
adityaashinde <adityashinde4580@gmail.com>
2024-03-05 10:51:02 +0530

    UI added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: QuizApp(),
     );
   }
@@ -34,6 +35,111 @@ class _QuizAppState extends State<QuizApp> {
         ),
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
+      ),
+      backgroundColor: Colors.deepPurple.shade200,
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 25,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Question's : ",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "1/5",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 300,
+                height: 40,
+                child: Text(
+                  "Questions",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // A
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              "A",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          // B
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              "B",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          // C
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              "C",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          // D
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              "D",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+      // Next
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.arrow_forward),
       ),
     );
   }
